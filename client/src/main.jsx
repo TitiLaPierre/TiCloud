a5699767de27eec376ff608ea98c57d49b265988
TitiLaPierre <52977803+TitiLaPierre@users.noreply.github.com>
2025-11-14 22:45:30 +0100

    Installed useQuery

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,13 @@ import { BrowserRouter } from "react-router-dom"
 import "@/css/main.css"
 
 import { App } from "@/App"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<BrowserRouter>
-    <App />
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
 </BrowserRouter>)
